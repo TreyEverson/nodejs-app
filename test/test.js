cb3c96482c7c8d5fc478fefdc8554c104350cb53
Trey Everson <teverson25@me.com>
2020-07-02 10:42:43 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
  it('respond with PIPELINE SUCESS', function(done) {
- request(app).get('/').expect('.... PIPELINE SUCESS ....', done);
+ request(app).get('/').expect('.... PIPELINE SUCCESS ....', done);
  });
 });
