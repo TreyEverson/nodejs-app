f2d0ca46e10dcd4575b976707ecc6dd65d4acb63
Trey Everson <teverson25@me.com>
2020-08-12 14:30:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('.... PIPELINE IS WORKING AFTER PIPELINE ✓ ....\n');
+ res.send('.... PIPELINE IS WORKING BEFORE CHANGES ✓ ....\n');
 });
 //listen to port 3001 by default
 app.listen(process.env.PORT || 3001);
